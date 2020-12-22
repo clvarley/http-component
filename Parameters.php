c17ee1312f36edee1e845a3b22777a61668876f7
clvarley <conorlukevarley@gmail.com>
2020-12-22 13:55:13 +0000

    Fleshed out Parameter container

--- Parameters.php
+++ Parameters.php
@@ -17,4 +17,57 @@ Class Parameters
      */
     protected $parameters = [];
 
+    /**
+     * Creates a new parameter bag from the (optionally) provided parameters
+     *
+     * @param array $parameters (Optional) Http parameters
+     */
+    public function __construct( array $parameters )
+    {
+        $this->parameters = $parameters;
+    }
+
+    /**
+     * Sets the value for the named parameter
+     *
+     * @param string $name Parameter name
+     * @param mixed $value Parameter value
+     * @return void        N/a
+     */
+    public function set( string $name, /* mixed */ $value ) : void
+    {
+        $this->parameters[$name] = $value;
+    }
+
+    /**
+     * Gets the value for the named parameter
+     *
+     * @param string $name Parameter name
+     * @return mixed|null  Parameter value
+     */
+    public function get( string $name ) // : mixed
+    {
+        return $this->parameters[$name] ?? null;
+    }
+
+    /**
+     * Checks to see if a named parameter has been set
+     *
+     * @param string $name Parameter name
+     * @return bool        Parameter set
+     */
+    public function has( string $name ) : bool
+    {
+        return \array_key_exists( $name, $this->parameters );
+    }
+
+    /**
+     * Gets all parameters
+     *
+     * @return array Parameter values
+     */
+    public function all() : array
+    {
+        return $this->parameters;
+    }
 }
