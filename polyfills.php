fd0a54f6fc87674836ec9fc6d2287e48a8547ba8
clvarley <conorlukevarley@gmail.com>
2020-12-29 17:49:33 +0000

    Provided basic polyfill for 'getallheaders'

--- polyfills.php
+++ polyfills.php
@@ -9,5 +9,32 @@ if ( !interface_exists( 'Stringable', false ) ) {
     {
         public function __toString() : string;
     }
-    
+
+}
+
+
+if ( !function_exists( 'getallheaders' ) ) {
+
+    /**
+     * Fetch all HTTP request headers
+     *
+     * @return array HTTP headers
+     */
+    function getallheaders() : array
+    {
+        if ( empty( $_SERVER ) ) {
+            return [];
+        }
+
+        $headers = [];
+
+        foreach ( $_SERVER as $name => $value ) {
+            if ( \substr( $name, 0, 5 ) === 'HTTP_' ) {
+                $headers[\substr( $name, 5 )] = $value;
+            }
+        }
+
+        return $headers;
+    }
+
 }
