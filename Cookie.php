c525fc87b2566ccfc3343492efcd90d6964e93c7
clvarley <conorlukevarley@gmail.com>
2021-01-04 18:06:08 +0000

    First attempt at Cookie properties

--- Cookie.php
+++ Cookie.php
@@ -10,4 +10,53 @@ namespace Http;
 Class Cookie
 {
 
+    /**
+     * The name of this cookie
+     *
+     * @var string $name Cookie name
+     */
+    protected $name;
+
+    /**
+     * The value of this cookie
+     *
+     * @var string $value Cookie value
+     */
+    protected $value;
+
+    /**
+     * Expiry date of this cookie
+     *
+     * @var int $expires Unix timestamp
+     */
+    protected $expires;
+
+    /**
+     * Allowed URL path
+     *
+     * @var string $path URL path
+     */
+    protected $path;
+
+    /**
+     * Allowed (sub)domains
+     *
+     * @var string $domain Allowed domains
+     */
+    protected $domain;
+
+    /**
+     * Only transmitt over HTTPS
+     *
+     * @var bool $secure HTTPS only
+     */
+    protected $secure;
+
+    /**
+     * Only readable via HTTP (no js)
+     *
+     * @var bool $httponly HTTP only
+     */
+    protected $httponly;
+
 }
